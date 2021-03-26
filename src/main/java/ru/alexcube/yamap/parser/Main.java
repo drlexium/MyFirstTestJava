e91631e298424829f09c829a732c84e1d55d21aa
Alex Cube <af8@af8.ru>
2021-03-26 10:55:14 +0300

    Попробую написать парсер

---
+++ src/main/java/ru/alexcube/yamap/parser/Main.java
@@ -0,0 +1,15 @@
+package ru.alexcube.yamap.parser;
+
+import org.junit.jupiter.api.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Старт парсера.");
+    }
+
+    @Test
+    public static void testStep1() {
+
+    }
+
+}
